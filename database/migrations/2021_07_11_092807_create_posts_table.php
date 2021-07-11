ba2d30b8af64d8f3b3c7321cbd13e14644be27e1
MrAuGir <augirard17@gmail.com>
2021-07-11 11:39:39 +0200

    factory Post

--- database/migrations/2021_07_11_092807_create_posts_table.php
+++ database/migrations/2021_07_11_092807_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticleTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateArticleTable extends Migration
      */
     public function up()
     {
-        Schema::create('article', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title',100);
-            $table->string('slug')->unique();
+            $table->string('title');
             $table->text('content');
             $table->timestamps();
         });
@@ -29,6 +28,6 @@ class CreateArticleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('article');
+        Schema::dropIfExists('posts');
     }
 }
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,6 @@
-<ul>
-    <li><a href="{{route('accueil')}}">Accueil</a></li>
-    <li><a href="{{route('contact')}}">Contact</a></li>
-</ul>
+<div class="nav">
+    <ul>
+        <li><a class="nav-item" href="{{route('accueil')}}">Accueil</a></li>
+        <li><a class="nav-item" href="{{route('contact')}}">Contact</a></li>
+    </ul>
+</div>
